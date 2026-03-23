bfdae6a99c8974651c9a67f800f98f0967db791a
yuchou <47242699+yuchou87@users.noreply.github.com>
2026-03-23 19:32:02 +0800

    chore(homebrew): sync formula to v0.1.10 (#20)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packaging/homebrew/Formula/hurl-lsp.rb
+++ packaging/homebrew/Formula/hurl-lsp.rb
@@ -2,22 +2,22 @@ class HurlLsp < Formula
   desc "Language Server Protocol implementation for Hurl"
   homepage "https://github.com/testmind-hq/hurl-lsp"
   license "MIT"
-  version "0.1.5"
+  version "0.1.10"
 
   on_macos do
     on_arm do
       url "https://github.com/testmind-hq/hurl-lsp/releases/download/v#{version}/hurl-lsp-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "5229b68016fe58ee81896c3556f4b2555b219ba8f951b6e411bde6ff8f7afc5a"
+      sha256 "9f3b44e4c7f1a010ca90b7f7ed88e9c9bec81ca965958e0fdff58aa41eea5281"
     end
     on_intel do
       url "https://github.com/testmind-hq/hurl-lsp/releases/download/v#{version}/hurl-lsp-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "cf8caffebe461be600d14a125487fdf2b3c94fc675515bd3524d3a61a4f8ba4b"
+      sha256 "d18b1e5350b59cfade853df1c06335e48a393327964eccc618d66dde6cd68378"
     end
   end
 
   on_linux do
     url "https://github.com/testmind-hq/hurl-lsp/releases/download/v#{version}/hurl-lsp-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "4987cea706e52b52106ec973bb22d07b2a87bad31140e512547ee121dbfc502c"
+    sha256 "9306443023447b0f153cd3af51e1e365406e404f825059c250834b3665590b51"
   end
 
   def install
