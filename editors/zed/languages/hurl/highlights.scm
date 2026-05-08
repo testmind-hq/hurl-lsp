d7a6d373888a680d6a10ef05892d1140c7d48601
yuchou <47242699+yuchou87@users.noreply.github.com>
2026-05-08 09:47:44 +0800

    fix(zed): upgrade to full Rust LSP extension with Zed-compatible config (#28)
    * fix(zed): upgrade extension to full Rust LSP extension with Zed-compatible config
    
    - Rewrite extension.toml: replace deprecated [language] block with [lib], [grammars.hurl], and [language_servers.hurl-lsp] sections
    - Fix [language_servers.hurl-lsp]: remove redundant languages field (use language = "Hurl" only)
    - Add languages/hurl/ with config.toml, highlights.scm, folds.scm, indents.scm
    - Fix injections.scm: rewrite capture names for Zed (@injection.content / #set! injection.language) instead of Neovim-style @json/@xml
    - Pin zed_extension_api to "0.7.0" instead of wildcard "*"
    - Ignore compiled WASM artifacts and nested grammar source clone in .gitignore
    
    * fix(zed): align highlight capture names and clean up extension.toml
    
    - Replace @float with @number.float for proper Zed theme token mapping
    - Replace @string.regex with @string.regexp to match Zed convention
    - Remove redundant languages = [] from [language_servers.hurl-lsp]
    - Fix folds.scm header comment (fold.scm -> folds.scm)

---
+++ editors/zed/languages/hurl/highlights.scm
@@ -0,0 +1,104 @@
+; highlights.scm
+
+"[Query]" @property
+"[QueryStringParams]" @property
+"[Form]" @property
+"[FormParams]" @property
+"[Multipart]" @property
+"[MultipartFormData]" @property
+"[Cookies]" @property
+"[Captures]" @property
+"[Asserts]" @property
+"[Options]" @property
+"[BasicAuth]" @property
+
+(comment) @comment @spell
+
+(key_string) @property
+(json_key_string) @property
+
+(value_string) @string
+(quoted_string) @string
+(json_string) @string
+(file_value) @string.special
+(regex) @string.regexp
+
+"\\" @string.escape
+(regex_escaped_char) @string.escape
+(quoted_string_escaped_char) @string.escape
+(key_string_escaped_char) @string.escape
+(value_string_escaped_char) @string.escape
+(oneline_string_escaped_char) @string.escape
+(multiline_string_escaped_char) @string.escape
+(filename_escaped_char) @string.escape
+(json_string_escaped_char) @string.escape
+
+(method) @type.builtin
+(multiline_string_type) @type
+
+"status" @function.builtin
+"url" @function.builtin
+"header" @function.builtin
+"cookie" @function.builtin
+"body" @function.builtin
+"xpath" @function.builtin
+"jsonpath" @function.builtin
+"regex" @function.builtin
+"variable" @function.builtin
+"duration" @function.builtin
+"sha256" @function.builtin
+"md5" @function.builtin
+"bytes" @function.builtin
+"daysAfterNow" @function.builtin
+"daysBeforeNow" @function.builtin
+"htmlEscape" @function.builtin
+"htmlUnescape" @function.builtin
+"decode" @function.builtin
+"format" @function.builtin
+"nth" @function.builtin
+"replace" @function.builtin
+"split" @function.builtin
+"toDate" @function.builtin
+"toInt" @function.builtin
+"urlEncode" @function.builtin
+"urlDecode" @function.builtin
+"count" @function.builtin
+
+(filter _) @attribute
+
+(version) @string.special
+
+"null" @constant.builtin
+(option (_ key: _ @constant.builtin))
+
+(boolean) @boolean
+
+(variable_name) @variable
+
+"==" @operator
+"!=" @operator
+">" @operator
+">=" @operator
+"<" @operator
+"<=" @operator
+(_ predicate_name: _ @keyword.operator)
+
+
+(integer) @number
+(float) @number.float
+(status) @number
+(json_number) @number.float
+
+":" @punctuation.delimiter
+"," @punctuation.delimiter
+
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+"{{" @punctuation.special
+"}}" @punctuation.special
+
+"base64," @string.special
+"file," @string.special
+"hex," @string.special
